2c8bced02a2c10924e9291a9ac7d59c07bbc863e
puri <puri@deshaw.com>
2016-09-02 05:24:50 -0400

    fixed a bug

--- src/main/java/com/kilo/MonitorInterceptor.java
+++ src/main/java/com/kilo/MonitorInterceptor.java
@@ -1,5 +1,6 @@
 package com.kilo;
 
+import java.util.HashSet;
 import java.util.concurrent.Callable;
 
 import net.bytebuddy.implementation.bind.annotation.Origin;
@@ -14,17 +15,21 @@ public class MonitorInterceptor {
 
 
     public static int count = 0;
+    static HashSet<String> set =  new HashSet<String>();
 
     @RuntimeType
     public static Object intercept(@Origin String method,
                                    @SuperCall Callable<?> zuper) throws Exception {
         try {
+            if(set.add(method))
+                System.out.println("Intercepting for methods: "+method);
             return zuper.call();
         } catch (Exception ex){
             ex.printStackTrace();
             throw ex;
         }finally {
-            if(count++ %1000_000 == 0)
+            count++;
+            if(count !=0 && count %1000_000 == 0)
                 System.out.println(count);
         }
     }
